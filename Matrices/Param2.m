afc5e2e973db3262db8ad87e223f167aae7e59cf
Miguel-Ribeiro-Desk <miguel.ribeiro.ist@gmail.com>
2017-11-15 23:19:17 +0100

    garch

---
+++ Matrices/Param2.m
@@ -0,0 +1,3 @@
+(* Created with the Wolfram Language for Students - Personal Use Only : www.wolfram.com *)
+{50, "Put", 50., 1., 40., 6.099309712648751*^-10, 0.00007925805178211124, 
+ 0.37143238357454034}

280b52d817c87c6f04691842afad1315e83062f3
Miguel-Ribeiro-Desk <miguel.ribeiro.ist@gmail.com>
2018-08-23 01:50:31 +0100

    clean up

--- Mathematica/Matrices/Param16.m
+++
@@ -1,2 +0,0 @@
-(* Created with the Wolfram Language for Students - Personal Use Only : www.wolfram.com *)
-{50000, "Put", 48., 1.25, 40.}
--- Mathematica/Matrices/Param2.m
+++
@@ -1,2 +0,0 @@
-(* Created with the Wolfram Language for Students - Personal Use Only : www.wolfram.com *)
-{50000, "Put", 12, 1., 153.99, 0.05, 0.9, 0.3}
